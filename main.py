765dfbebe2f7ae588a521465968fe95d294bb96e
Victor Kaklamanis <vkaklam@dome.imgln.net>
2022-08-10 17:23:55 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,244 @@
+import project_classes
+
+NEW_LINE = "\n"
+INPUT_DECO = "\n> "
+DECO = "-" * 45
+
+
+def coordinate_combat_phase(
+        char: project_classes.Character,
+        enemy: project_classes.Character,
+        printing=True) -> project_classes.Character:
+
+    if printing:
+        display_battle_start(char, enemy)
+
+    i = 1
+    while True:
+
+        if printing:
+            print(f"Round {i}")
+            print(DECO)
+
+        combat_options = {
+            0: char.attack,
+            1: char.flee,
+        }
+
+        display_combat_options()
+
+        choice = int(input("> "))
+
+        combat_options[choice](enemy)
+
+        match choice:
+
+            case 0:
+
+                if printing:
+                    print(
+                        f"{char.name} hits {enemy.name} for {char.weapon.damage} damage ({char.weapon.name})."
+                    )
+
+            case 1:
+
+                text = combat_options[choice](enemy)[0]
+                code = combat_options[choice](enemy)[1]
+
+                print(NEW_LINE + text)
+
+                if code == 0:
+                    # todo: implement flee success
+                    ...
+
+                else:
+                    continue
+
+        if not enemy.is_alive():
+
+            if printing:
+                print(display_battle_recap(enemy))
+
+            return enemy
+
+        # todo: implement AI call here (deciding NPC combat actions)
+        enemy.attack(char)
+
+        if printing:
+
+            print(
+                f"{enemy.name} hits {char.name} for {enemy.weapon.damage} damage ({enemy.weapon.name})."
+            )
+
+            print()
+
+            display_combatants_health(char, enemy)
+
+            print()
+
+        if not char.is_alive():
+
+            if printing:
+                print(display_battle_recap(char))
+
+            return char
+
+        i += 1
+
+
+def display_combat_options() -> None:
+
+    print(
+        NEW_LINE
+        + "Next action ?"
+        + "\n0: ATTACK\t1: FLEE"
+    )
+
+
+def display_battle_start(char: project_classes.Character, opp: project_classes.Character):
+
+    print(
+        NEW_LINE
+        + f"FIGHT BREAKOUT: {char.name} (LVL {char.level}, {char.hp}/{char.hp_max} HP)"
+        + f" VS {opp.name} (LVL {opp.level}, {opp.hp}/{opp.hp_max} HP)"
+        + NEW_LINE
+    )
+
+
+def display_battle_recap(defeated_char: project_classes.Character) -> str:
+    return f"{defeated_char.name} has been defeated!"
+
+
+def display_xp_awarded(winning_char: project_classes.Character, defeated_char: project_classes.Character) -> str:
+    return f"{winning_char.name} is awarded {defeated_char.award_xp()} XP."
+
+
+def display_combatants_health(char: project_classes.Character, opp: project_classes.Character):
+    print(char)
+    print(opp)
+
+
+def check_xp_values(char: project_classes.Character, number_of_levels: int) -> dict:
+
+    dict_of_values = {}
+
+    i = 1
+    for level in range(1, number_of_levels + 1):
+
+        char.set_xp_bar()
+        xp_needed_to_next_level = char.xp_bar
+
+        dict_of_values[i] = char.xp_bar
+
+        char.xp += xp_needed_to_next_level
+
+        if char.is_ready_to_level_up():
+            char.level_up()
+
+        i += 1
+
+    return dict_of_values
+
+
+def display_xp_values(values_dict: dict):
+
+    for key, value in values_dict.items():
+        print(
+            f"Level {key}: {value} EXP"
+        )
+
+
+def check_xp_yield_values(char: project_classes.Character, number_of_levels: int, level_dict=None):
+
+    for level in range(1, number_of_levels+1):
+
+        if type(level_dict) == dict:
+            print(
+                f"Level: {level} (XP needed for next level: {list(level_dict.values())[level - 1]})"
+            )
+        else:
+            print(
+                f"Level: {level}"
+            )
+
+        for tier in range(1, 4):
+
+            char.tier = tier
+
+            char.set_xp_yield()
+
+            print(
+                f"\tTier {tier}: {char.xp_yield}"
+            )
+
+        char.level += 1
+
+
+def calc_percentage(a, b) -> float:
+    return round((a / b) * 100, 2)
+
+
+# not implemented
+def load_game() -> str:
+    return "Loading recent save."
+
+
+def main():
+
+    # Weapons
+    unarmed = project_classes.Weapon()
+    unarmed.name = "Unarmed"
+    unarmed.weapon_type = "Regular"
+    unarmed.damage = 1
+
+    bkh = project_classes.Weapon()
+    bkh.name = "Black Knight Halberd"
+    bkh.weapon_type = "Special"
+    bkh.damage = 7
+
+    rd = project_classes.Weapon()
+    rd.name = "Rusty Dagger"
+    rd.weapon_type = "Regular"
+    rd.damage = 3
+
+    # Hero
+    hero_name = "Sire McDoughNat"
+    hero = project_classes.Character(hero_name, unarmed)
+    hero.hp = 48
+    hero.hp_max = 48
+    hero.stats["Speed"] = 5
+
+    # Enemy
+    enemy_name = "Goblin"
+    goblin = project_classes.Character(enemy_name, unarmed)
+    goblin.hp = 15
+    goblin.hp_max = 15
+    goblin.set_xp_yield()
+    goblin.stats["Speed"] = 5
+
+    print("Goblin XP YIELD:", goblin.xp_yield)
+
+    hero.equip_weapon(bkh)
+    goblin.equip_weapon(rd)
+
+    printing = True
+    xp_yield = coordinate_combat_phase(hero, goblin, printing)
+
+    hero.xp += xp_yield
+
+    check_level_up = False
+    if check_level_up:
+        print(f"\n{hero.name} is ready to level up ?", hero.is_ready_to_level_up())
+
+    # Value testing
+    # b = check_xp_values(hero, 60)
+    # display_xp_values(b)
+
+    # hero.level = 1
+
+    # print()
+    # check_xp_yield_values(hero, 60, b)
+
+
+if __name__ == "__main__":
+    main()
---
+++ project_classes.py
@@ -0,0 +1,114 @@
+
+class Weapon:
+    name: str
+    weapon_type: str
+    damage: int
+
+    def __repr__(self) -> str:
+        return f"WEAPON: [{self.name}: {self.weapon_type} CLASS, {self.damage} DMG]"
+
+
+class Character:
+
+    name: str
+
+    hp: int
+    hp_max: int
+
+    alive: bool
+
+    weapon: Weapon
+
+    level: int
+
+    xp: int
+    xp_bar: int
+    xp_yield: int
+
+    tier: int
+
+    stats: dict
+
+    def __init__(self, name: str, weapon: Weapon):
+        self.name = name
+        self.alive = True
+        self.level = 1
+        self.xp = 0
+        self.set_xp_bar()
+        self.xp_yield = 1
+        self.tier = 1
+        self.stats = {}
+        self.equip_weapon(weapon)
+
+    def __repr__(self) -> str:
+        return f"{self.name}:" + " " + self.show_health()
+
+    def show_health(self) -> str:
+        return f"{self.hp}/{self.hp_max} HP"
+
+    def show_weapon(self) -> str:
+        return f"[Weapon: {self.weapon.name}, {self.weapon.damage} DMG]"
+
+    def show_health_and_weapon(self) -> str:
+        return self.__repr__() + ", " + self.show_weapon()
+
+    def equip_weapon(self, weapon: Weapon) -> None:
+        self.weapon = weapon
+
+    def attack(self, other: any) -> None:
+        other.hp -= self.weapon.damage
+
+        if not other.is_alive():
+            other.alive = False
+
+    def is_alive(self) -> bool:
+        return self.hp > 0
+
+    def is_ready_to_level_up(self) -> tuple:
+        return self.xp >= self.xp_bar, f"{self.name}: {self.xp}/{self.xp_bar} XP"
+
+    def set_xp_bar(self) -> None:
+
+        """
+        padding: 8; scale: 7
+
+        Level 1: 63 EXP
+        Level 2: 140 EXP
+        Level 3: 231 EXP
+        Level 4: 336 EXP
+        Level 5: 455 EXP
+        Level 6: 588 EXP
+        Level 7: 735 EXP
+        Level 8: 896 EXP
+        Level 9: 1071 EXP
+        Level 10: 1260 EXP
+        """
+
+        padding = 8
+        scale = 7
+
+        self.xp_bar = (self.level + padding) * (self.level * scale)
+
+    def set_xp_yield(self) -> None:
+        padding = 5
+        self.xp_yield = (self.level + padding) * (self.tier * self.level)
+
+    def award_xp(self) -> int:
+        return self.xp_yield
+
+    def level_up(self) -> None:
+
+        if self.xp > self.xp_bar:
+            self.xp -= self.xp_bar
+            self.level += 1
+
+        elif self.xp == self.xp_bar:
+            self.xp = 0
+            self.level += 1
+
+    def flee(self, other: any) -> tuple:
+
+        if self.stats["Speed"] > other.stats["Speed"]:
+            return "Fled combat.", 0
+        else:
+            return "Cannot run away!", 1
