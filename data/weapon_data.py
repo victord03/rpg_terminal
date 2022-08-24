bfbdc1b74180e955689a279dc16f930753e5a0ad
victord03 <vic.kaklamanis@gmail.com>
2022-08-24 22:33:02 +0300

    Added template in data.weapon_data.py in order to populate quickly with additional weapons.

--- data/weapon_data.py
+++ data/weapon_data.py
@@ -1,9 +1,14 @@
 """This file will hold all weapon data as a local database"""
 
+# document template "": {"pn": "", "damage": 0},
+
 # pn = printable name
 weapons = {
     "unarmed": {"pn": "Unarmed", "damage": 1},
     "rusty_dagger": {"pn": "Rusty Dagger", "damage": 2},
-    "black_knight_halberd": {"pn": "Black Knight Halberd", "damage": 7},
     "short_sword": {"pn": "Short Sword", "damage": 3},
+    "long_sword": {"pn": "Long Sword", "damage": 4},
+    "": {"pn": "", "damage": 0},
+    "black_knight_halberd": {"pn": "Black Knight Halberd", "damage": 7},
 }
+
